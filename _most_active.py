bf697da9901f336b32b63a4ad58bf760623d24b7
dantesbytes <dantesbytes.dev@yahoo.com>
2024-09-04 20:40:56 -0400

    fc

--- _most_active.py
+++ _most_active.py
@@ -10,9 +10,9 @@ df = pd.read_csv(url)
 
 df_filtered = df[(df['year'] >= 2000) & (df['year'] <= 2024)]
 
-max_active_years = df_filtered['years_active'].max()
+max_years = df_filtered['years_active'].max()
 
-top_players = df_filtered[df_filtered['years_active'] == max_active_years].sort_values('year')
+top_players = df_filtered[df_filtered['years_active'] == max_years].sort_values('year')
 
-print(f"Player(s) with the most active years ({max_active_years}) from draft classes 2000-2024:")
+print(f"Player(s) with the most active years ({max_years}) from draft classes 2000-2024:")
 print(top_players[['player', 'year', 'years_active']])
