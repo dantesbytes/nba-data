dc35090325aa53edc9b694ce3d2fe5f907654013
dantesbytes <dantesbytes.dev@yahoo.com>
2024-09-05 17:40:15 -0400

    players with the most winds

--- _most_active.py
+++ _most_active.py
@@ -16,3 +16,14 @@ top_players = df_filter[df_filter['years_active'] == max_years].sort_values('yea
 
 print(f"Player(s) with the most active years ({max_years}) from draft classes 2000-2024:")
 print(top_players[['player', 'year', 'years_active']])
+
+print(df.head())
+
+
+max_winshares = df_filter['win_shares'].max()
+
+top_windshares_players = df_filter[df_filter['win_shares'] == max_winshares].sort_values('year')
+
+print(f"player(s) with the most wind shares ({max_winshares}) from 2000 - 2024")
+
+print(top_windshares_players[['player', 'year','years_active']])
